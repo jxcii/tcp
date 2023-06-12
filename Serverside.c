3c8b5f713698103f72dfec78662475907d1a173a
jxcii <97617863+jxcii@users.noreply.github.com>
2023-06-12 16:19:03 +0500

    Update Serverside.c

--- Serverside.c
+++ Serverside.c
@@ -25,4 +25,33 @@ void func(int connfd){
     }
   }
 }
-S
+int main(){
+  int sockfd, connfd, len;
+  struct sockaddr_in servaddr, cli;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(socketfd==-1){
+    printf("socket creation failed...\n");
+    exit(0);
+  } else printf("Socket successfully created");
+  bzero(&servaddr, sizeof(servaddr));
+  //IP, Port 
+  servaaddr.sin_family = AF_INET;
+  servaaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaaddr.sin_port = htons(PORT);
+  if(bind(sockfd, (SA*)&servaaddr, sizeof(servaaddr))!=0){
+    printf("socket bind failed\n");
+    exit(0);
+  } else printf("socket successfully binded\n");
+  if(listen(sockfd, 5)!=0){
+    printf("socket listen failed...\n");
+    exit(0);
+  } else prinf("socket successfully listened\n");
+  len = sizeof(cli);
+  connfd = accept(sockfd, (SA*)&cli, &len);
+  if(connfd<0){
+    printf("server accept failed\n");
+    exit(0);
+  }else print("server accept the client...\n");
+  func(sockfd);
+  close(sockfd);
+}
